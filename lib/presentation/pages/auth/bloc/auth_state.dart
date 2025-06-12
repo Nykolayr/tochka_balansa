b3aa22c27f62e1d0dee455ca4c223be665bbd547
NykolayR <nykolayr@gmail.com>
2025-06-12 22:36:31 +0700

    feat: add user

--- lib/presentation/pages/auth/bloc/auth_state.dart
+++ lib/presentation/pages/auth/bloc/auth_state.dart
@@ -1,10 +1,63 @@
 part of 'auth_bloc.dart';
 
-sealed class AuthState extends Equatable {
-  const AuthState();
-  
+class AuthState extends Equatable {
+  final User user;
+  final AuthStatus status;
+  final String error;
+  final bool isReg;
+  final String code;
+
+  const AuthState({
+    required this.user,
+    required this.status,
+    required this.error,
+    required this.isReg,
+    required this.code,
+  });
+
+  factory AuthState.initial() => AuthState(
+    user: User.initial(),
+    status: AuthStatus.initial,
+    error: '',
+    isReg: false,
+    code: '',
+  );
+
+  AuthState copyWith({
+    User? user,
+    AuthStatus? status,
+    String? error,
+    String? phone,
+    bool? isReg,
+    String? code,
+  }) {
+    return AuthState(
+      user: user ?? this.user,
+      status: status ?? this.status,
+      error: error ?? this.error,
+      isReg: isReg ?? this.isReg,
+      code: code ?? this.code,
+    );
+  }
+
   @override
-  List<Object> get props => [];
+  List<Object?> get props => [user, status, error, isReg, code];
 }
 
-final class AuthInitial extends AuthState {}
+enum AuthStatus {
+  initial,
+  loading,
+  successEnter,
+  successRegister,
+  successCode,
+  successAccept,
+  error;
+
+  bool get isSuccessEnter => this == AuthStatus.successEnter;
+  bool get isSuccessRegister => this == AuthStatus.successRegister;
+  bool get isSuccessCode => this == AuthStatus.successCode;
+  bool get isSuccessAccept => this == AuthStatus.successAccept;
+  bool get isError => this == AuthStatus.error;
+  bool get isLoading => this == AuthStatus.loading;
+  bool get isInitial => this == AuthStatus.initial;
+}
---
+++ lib/presentation/pages/auth/code_page.dart
@@ -0,0 +1,93 @@
+import 'package:equatable/equatable.dart';
+import 'package:tochka_balansa/data/models/gender.dart';
+
+/// Модель пользователя
+class User extends Equatable {
+  final int id;
+  final String name;
+  final DateTime birthDate;
+  final int age;
+  final String email;
+  final Gender gender;
+  final double initialWeight;
+
+  bool get isReg => name.isNotEmpty;
+
+  const User({
+    required this.id,
+    required this.name,
+    required this.birthDate,
+    required this.age,
+    required this.email,
+    required this.gender,
+    required this.initialWeight,
+  });
+
+  factory User.initial() => User(
+    id: 0,
+    name: '',
+    birthDate: DateTime.now(),
+    age: 0,
+    email: '',
+    gender: Gender.female,
+    initialWeight: 0.0,
+  );
+
+  factory User.fromJson(Map<String, dynamic> json) {
+    return User(
+      id: json['id'] ?? 0,
+      name: json['name'] ?? '',
+      birthDate: DateTime.tryParse(json['birth_date'] ?? '') ?? DateTime.now(),
+      age: json['age'] ?? 0,
+      email: json['email'] ?? '',
+      gender: Gender.values.firstWhere(
+        (g) => g.toString() == 'Gender.${json['gender']}',
+        orElse: () => Gender.female,
+      ),
+      initialWeight: (json['initial_weight'] ?? 0).toDouble(),
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'name': name,
+      'birth_date': birthDate.toIso8601String(),
+      'age': age,
+      'email': email,
+      'gender': gender.name,
+      'initial_weight': initialWeight,
+    };
+  }
+
+  User copyWith({
+    int? id,
+    String? name,
+    DateTime? birthDate,
+    int? age,
+    String? email,
+    Gender? gender,
+    double? initialWeight,
+  }) {
+    return User(
+      id: id ?? this.id,
+      name: name ?? this.name,
+      birthDate: birthDate ?? this.birthDate,
+      age: age ?? this.age,
+      email: email ?? this.email,
+      gender: gender ?? this.gender,
+      initialWeight: initialWeight ?? this.initialWeight,
+    );
+  }
+
+  @override
+  List<Object?> get props => [
+    id,
+    name,
+    birthDate,
+    age,
+    email,
+    gender,
+    initialWeight,
+  ];
+}
